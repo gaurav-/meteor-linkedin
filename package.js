20160824f51e01f005107f789002abeb738e46a1
Gaurav <gaurav.thapa@peerlyst.com>
2016-02-02 15:22:47 +0530

    excluding files not needed in our setup

--- package.js
+++ package.js
@@ -18,9 +18,11 @@ Package.onUse(function(api) {
 
   api.export('LinkedIn');
 
+/* Don't need these files for non-Blaze env
   api.addFiles(
     ['linkedin_configure.html', 'linkedin_configure.js'],
     'client');
+*/
   api.addFiles('linkedin_common.js', ['client', 'server']);
   api.addFiles('linkedin_server.js', 'server');
   api.addFiles('linkedin_client.js', 'client');
